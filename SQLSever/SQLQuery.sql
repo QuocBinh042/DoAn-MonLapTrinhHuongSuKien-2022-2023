71f76314244fc3769e12b4b4b2bf5793578bd811
vuongng2212 <kudoyaung@yahoo.com>
2023-04-27 19:42:17 +0700

    update ERD

--- SQLSever/SQLQuery.sql
+++ SQLSever/SQLQuery.sql
@@ -1,10 +1,13 @@
-create database QuanLyKhachSan
+﻿create database QuanLyKhachSan
 go
+--drop database QuanLyKhachSan
 use QuanLyKhachSan
 go
+
+--Create table
 create table DichVu(
-	MaDV nvarchar(50) not null primary key,
-	TenDV nvarchar(50),
+	MaDichVu nvarchar(50) not null primary key,
+	TenDichVu nvarchar(50),
 	Gia float
 )
 create table Phong(
@@ -13,39 +16,81 @@ create table Phong(
 	LoaiPhong nvarchar(50),
 	GiaPhong float,
 	MoTa nvarchar(200),
-	TinhTrang bit
+	--Tinh trạng còn trống hay đã đặt phòng
+	--0: còn trống
+	--1: đã đặt
+	TinhTrang bit default 0
 )
-
+create table HoaDonDichVuPhong(
+	MaPhong nvarchar(50) not null,
+	MaDichVu nvarchar(50) not null,
+	SoLuong int default 0,
+	-------------------------
+	--Thành tiền dịch vụ = SUM(Gia*SoLuong)
+	--Giá(DichVu)
+	--SoLuong(HoaDonDichVuPhong)
+	ThanhTienDichVu float,--Dynamic
+	-------------------------
+	CONSTRAINT fk_maphong FOREIGN KEY (MaPhong) REFERENCES Phong(MaPhong),
+	CONSTRAINT fk_madichvu FOREIGN KEY (MaDichVu) REFERENCES DichVu(MaDichVu)
+)
 create table KhachHang(
 	MaKhachHang nvarchar(50) not null primary key,
-	TenKhachHang nvarchar(50),
-	CMT nvarchar(50),
-	SDT nvarchar(10),
+	TenKhachHang nvarchar(50) not null,
+	CMT nvarchar(50) not null,
+	SDT nvarchar(10) not null,
 	Gmail nvarchar(50)
 )
 
 create table NhanVien(
 	MaNhanVien nvarchar(50) not null primary key,
-	HoTen nvarchar(50),
-	CMT nvarchar(50),
-	SDT nvarchar(10),
+	HoTen nvarchar(50) not null,
+	Pwd nvarchar(50) not null,
+	--Chức vụ
+	--0: nhân viên bình thường
+	--1: quản lý
+	ChucVu bit not null,
+	--Giới tính
+	GioiTinh char(3),
+	CMT nvarchar(50) not null,
+	SDT char(10) not null,
 	Gmail nvarchar(50),
-	ChucVu nvarchar(50)
+	DiaChi nvarchar(200)
 )
 
 create table HoaDonThanhToan(
 	MaHoaDon nvarchar(50) not null primary key,
 	NgayThanhToan datetime,
-	HinhThucThanhToan nvarchar(50),
-	TongThanhToan float,
+	--Hình thức thanh toán
+	--0: tiền mặt
+	--1: chuyển khoản
+	HinhThucThanhToan bit,
+
+	---------------------------------------------
+	--Thành tiền phòng = số ngày ở(PhieuDatPhong) * giá phòng(Phong)
+	ThanhTienPhong float,--Dynamic
+	--Tổng thanh toán = SUM(ThanhTienDichVuPhong) (HoaDonDichVuPhong) + ThanhTienPhong
+	TongThanhToan float,--Dynamic
+	---------------------------------------------
+
 	GhiChu nvarchar(200)
 )
-
 create table PhieuDatPhong(
 	MaDatPhong nvarchar(50) not null primary key,
-	NgayDatPhong date,
-	NgayDen date,
-	NgayDi date,
+	-----FOREIGN KEY
+	MaNV nvarchar(50) not null,
+	MaPhong nvarchar(50) not null,
+	IDNguoiDatPhong nvarchar(50) not null,
+	MaHoaDon nvarchar(50) not null,
+	-----
+	NgayDatPhong datetime,
+	NgayCheckIn datetime,
+	NgayCheckOut datetime,
 	SoNguoi int,
-	GiaTien float
+	GhiChu nvarchar(50)
+
+	CONSTRAINT fk_nv FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNhanVien),
+	CONSTRAINT fk_mp FOREIGN KEY (MaPhong) REFERENCES Phong(MaPhong),
+	CONSTRAINT fk_kh FOREIGN KEY (IDNguoiDatPhong) REFERENCES KhachHang(MaKhachHang),
+	CONSTRAINT fk_hd FOREIGN KEY (MaHoaDon) REFERENCES HoaDonThanhToan(MaHoaDon)
 )
