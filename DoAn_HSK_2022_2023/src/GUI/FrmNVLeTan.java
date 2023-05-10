0b67f3f7a3e4538c217269ec5f164da66709f395
QuocBinh042 <tranlequocbinh2003@gmail.com>
2023-05-10 20:14:07 +0700

    Update GUI Le Tan

--- DoAn_HSK_2022_2023/src/GUI/FrmNVLeTan.java
+++ DoAn_HSK_2022_2023/src/GUI/FrmNVLeTan.java
@@ -11,78 +11,90 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 import javax.swing.table.DefaultTableModel;
 
-public class FrmNVLeTan extends JFrame implements ActionListener {
-	private JButton btnDSPhong, btnDSKhachHang, btnDSPhieuDatPhong, btnDSHoaDonThanhToan,btnHoaDonDichVuPhong, btnDichVu, btnThoat;
-	private JButton btnDSCheckin, btnDSCheckout;
-	private FrmPhieuDatPhong frmPDP = new FrmPhieuDatPhong();
-	private FrmHoaDonThanhToan frmHDTT = new FrmHoaDonThanhToan();
-	private FrmPhong frmDSP = new FrmPhong();
-	private FrmKhachHang frmDSKH = new FrmKhachHang();
-	private FrmDichVu frmDV = new FrmDichVu();
+public class FrmNVLeTan extends JFrame implements MenuListener {
+	private FrmPhieuDatPhong frmPhieuDatPhong = new FrmPhieuDatPhong();
+	private FrmHoaDonThanhToan frmHoaDonThanhToan = new FrmHoaDonThanhToan();
+	private FrmPhong frmPhong = new FrmPhong();
+	private FrmKhachHang frmKhachHang = new FrmKhachHang();
+	private FrmDichVu frmDichVu = new FrmDichVu();
 	private FrmHoaDonDichVuPhong frmHDDVP = new FrmHoaDonDichVuPhong();
 	private DefaultTableModel tableModel;
 	private JTable table;
-
+	private JMenuBar mnubar = new JMenuBar();
+	private JMenu mnuPhong, mnuKhachHang, mnuPhieuDatPhong, mnuHoaDonThanhToan,mnuHoaDonDichVuPhong, mnunDichVu, mnuTrangChu, mnuThoat;
+	private JList<String> myList;
 	public FrmNVLeTan() {
 		setTitle("QUẢN LÝ THÔNG TIN ĐẶT PHÒNG KHÁCH SẠN");
-		setSize(1020, 600);
+		setSize(1350, 700);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		createGUI();
 	}
 
 	private void createGUI() {
-		JPanel pnlNorth;
-		add(pnlNorth = new JPanel(), BorderLayout.NORTH);
-		JLabel lblTilte;
-		pnlNorth.add(lblTilte = new JLabel("QUẢN LÝ THÔNG TIN ĐẶT PHÒNG KHÁCH SẠN"));
-		lblTilte.setFont(new Font("Arial", Font.BOLD, 20));
-		lblTilte.setForeground(Color.BLUE);
+//		JPanel pnlNorth;
+//		add(pnlNorth = new JPanel(), BorderLayout.NORTH);
+//		JLabel lblTilte;
+//		pnlNorth.add(lblTilte = new JLabel("QUẢN LÝ THÔNG TIN ĐẶT PHÒNG KHÁCH SẠN"));
+//		lblTilte.setFont(new Font("Arial", Font.BOLD, 20));
+//		lblTilte.setForeground(Color.BLUE);
 
+		setJMenuBar(mnubar);
+		mnubar.add(mnuTrangChu = new JMenu("Trang chủ"));
+		mnubar.add(mnuPhong = new JMenu("Phòng"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnuKhachHang = new JMenu("Khách hàng"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnunDichVu = new JMenu("Dịch vụ"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnuPhieuDatPhong = new JMenu("Phiếu đặt phòng"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnuHoaDonDichVuPhong = new JMenu("Hoá đơn dịch vụ phòng"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnuHoaDonThanhToan = new JMenu("Hoá đơn thanh toán"));
+		mnubar.add(mnuTrangChu = new JMenu(""));
+		mnubar.add(mnuThoat = new JMenu("Thoát"));
+		JPanel pnlWest;
+		DefaultListModel<String> model = new DefaultListModel<>();
+        // add element to model
+        model.addElement("Danh sách checkin ");
+        model.addElement("Danh sách checkout");
+        // set model to JList
+        myList = new JList<String>(model);
+        add(pnlWest = new JPanel(), BorderLayout.WEST);
+        pnlWest.setBorder(new EmptyBorder(0, 10, 10, 10));
+        pnlWest.setBorder(BorderFactory.createTitledBorder("Danh sách"));
+        pnlWest.add(myList);
+       
 		Box bb = Box.createVerticalBox();
 		bb.setBorder(BorderFactory.createEtchedBorder());
 		Box b1 = Box.createHorizontalBox();
 		Box b2 = Box.createVerticalBox();
 		Box b = Box.createHorizontalBox();
-		b1.add(btnDSPhong = new JButton("PHÒNG"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnDSKhachHang = new JButton("THÔNG TIN KHÁCH HÀNG"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnDichVu = new JButton("DỊCH VỤ"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnDSPhieuDatPhong = new JButton("PHIẾU ĐẶT PHÒNG"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnDSHoaDonThanhToan = new JButton("HOÁ ĐƠN THANH TOÁN"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnHoaDonDichVuPhong = new JButton("HOÁ ĐƠN DỊCH VỤ PHÒNG"));
-		b1.add(Box.createHorizontalStrut(10));
-		b1.add(btnThoat = new JButton("THOÁT"));
-		btnThoat.setBackground(Color.RED);
-		btnThoat.setForeground(Color.WHITE);
 		bb.add(b1);
 		add(bb, BorderLayout.CENTER);
-		b2.setBorder(BorderFactory.createTitledBorder("Danh sách"));
-		b2.add(btnDSCheckin = new JButton("Danh sách chờ đến "));
-		b2.add(btnDSCheckout = new JButton("Danh sách checkout"));
-		b2.add(Box.createVerticalGlue());
 
-		b.add(b2);
 		bb.add(Box.createVerticalStrut(10));
 		bb.add(b);
-		String[] headers = "Mã khách hàng;Họ tên; Mã đặt phòng".split(";");
+		String[] headers = "Mã đặt phòng; Họ tên khách hàng; Tên phòng; Tình trạng ở; Ngày đến; Ngày đi; Ghi chú".split(";");
 		tableModel = new DefaultTableModel(headers, 0);
 		JScrollPane scroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -93,14 +105,13 @@ public class FrmNVLeTan extends JFrame implements ActionListener {
 		b.add(scroll);
 		bb.add(b);
 
-		btnDSPhong.addActionListener(this);
-		btnDSKhachHang.addActionListener(this);
-		btnDSPhieuDatPhong.addActionListener(this);
-		btnDSHoaDonThanhToan.addActionListener(this);
-		btnHoaDonDichVuPhong.addActionListener(this);
-		btnDichVu.addActionListener(this);
-		btnThoat.addActionListener(this);
-
+		mnuPhong.addMenuListener(this);
+		mnuKhachHang.addMenuListener(this);
+		mnunDichVu.addMenuListener(this);
+		mnuHoaDonDichVuPhong.addMenuListener(this);
+		mnuPhieuDatPhong.addMenuListener(this);
+		mnuHoaDonThanhToan.addMenuListener(this);
+		mnuThoat.addMenuListener(this);
 	}
 
 	public static void main(String[] args) {
@@ -108,29 +119,41 @@ public class FrmNVLeTan extends JFrame implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void menuSelected(MenuEvent e) {
+		// TODO Auto-generated method stub
 		Object o = e.getSource();
-		if (o.equals(btnDSPhong)) {
-			frmDSP.setVisible(true);
+		if (o.equals(mnuPhong)) {
+			frmPhong.setVisible(true);
 		}
-		if (o.equals(btnDichVu)) {
-			frmDV.setVisible(true);
+		if (o.equals(mnuKhachHang)) {
+			frmKhachHang.setVisible(true);
 		}
-		if (o.equals(btnDSKhachHang)) {
-			frmDSKH.setVisible(true);
+		if (o.equals(mnunDichVu)) {
+			frmDichVu.setVisible(true);
 		}
-		if (o.equals(btnDSPhieuDatPhong)) {
-			frmPDP.setVisible(true);
+		if (o.equals(mnuPhieuDatPhong)) {
+			frmPhieuDatPhong.setVisible(true);
 		}
-		if (o.equals(btnDSHoaDonThanhToan)) {
-			frmHDTT.setVisible(true);
-		}
-		if (o.equals(btnHoaDonDichVuPhong)) {
+		if (o.equals(mnuHoaDonDichVuPhong)) {
 			frmHDDVP.setVisible(true);
 		}
-		if (o.equals(btnThoat)) {
-			System.exit(0);
-			;
+		if (o.equals(mnuHoaDonThanhToan)) {
+			frmHoaDonThanhToan.setVisible(true);
 		}
+		if (o.equals(mnuThoat)) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void menuDeselected(MenuEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void menuCanceled(MenuEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
